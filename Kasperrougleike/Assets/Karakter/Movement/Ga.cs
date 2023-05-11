1e537ef700c9149d7e0d864da4a3eea4483d44d3
Bobles06 <Elias.gjerde@gmail.com>
2023-05-11 21:11:05 +0200

    startet på movement

---
+++ Kasperrougleike/Assets/Karakter/Movement/Ga.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ga : MonoBehaviour
+{
+    public float movementspeed;
+    public Rigidbody2D rb;
+    private Vector2 move;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        
+    }
+    void FixedUpdate() {
+        Move();
+    }
+    void ProcessInputs(){
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        move = new Vector2(moveX, moveY);
+    }
+    void Move()
+    {
+        rb.velocity = new Vector2(move.x * movementspeed, move.y * movementspeed);
+    }
+}
