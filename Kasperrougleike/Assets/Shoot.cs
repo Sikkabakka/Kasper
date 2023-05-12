1c277f9092d39c1cb3754092206ad047554db8ad
kaloa <kasper.ko.sv@iCloud.com>
2023-05-13 00:39:34 +0200

    Shooting

--- Kasperrougleike/Assets/Shoot.cs
+++ Kasperrougleike/Assets/Shoot.cs
@@ -8,10 +8,13 @@ public class Shoot : MonoBehaviour
     private Vector2 direction;
     public GameObject Bullet;
     public float BulletSpeed;
-    
+    public bool ReadyToShoot;
+    public float waitTime;  
+    public PlayerStats PS;
+
     void Start()
     {
-        
+     ReadyToShoot = true;   
     }
 
     // Update is called once per frame
@@ -20,18 +23,32 @@ public class Shoot : MonoBehaviour
     Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     direction = mousePos - (Vector2)ShootPoint.position;
     FaceMouse();      
-    if (Input.GetButtonDown("Fire1")){
+    if (Input.GetButtonDown("Fire1") && (ReadyToShoot == true)&&(PS.PlayerAmmo > 0)){
         Shooting();
+        PS.PlayerAmmo -= 1;
+        StartCoroutine(Fire(waitTime));
+        
     }
     }
     void FaceMouse(){
         ShootPoint.transform.right = direction;
     }
     void Shooting(){
-        Bullet = Instantiate(Bullet, ShootPoint.position,ShootPoint.rotation);
-        Bullet.GetComponent<Rigidbody2D>().AddForce(ShootPoint.transform.right*BulletSpeed);
+        GameObject bulletPrefab = Instantiate(Bullet, ShootPoint.position,ShootPoint.rotation);
+        bulletPrefab.GetComponent<Rigidbody2D>().AddForce(ShootPoint.transform.right*BulletSpeed);
+        bulletPrefab.AddComponent<Destroy>();
+        
 
         
     }
+    private IEnumerator Fire(float waitTime){
+        
+            ReadyToShoot = false;
+            yield return new WaitForSeconds(waitTime);
+            ReadyToShoot = true;
+        
+        
+    }
+
     
 }
